9c1e4e68cc7bdc53baa3f3918bf0c58085143976
shahidx0x <dev.imshahid@gmail.com>
2024-09-10 05:22:15 +0600

    update page.tsx, page.tsx, package.json and pnpm-lock.yaml

--- apps/web/src/app/dashboard/company/page.tsx
+++ apps/web/src/app/dashboard/company/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import { DataTableCompany } from "./components/data-table";
+
 const page = () => {
-  return <div>Manage Company</div>;
+  return (
+    <div className="p-5">
+      <DataTableCompany />
+    </div>
+  );
 };
 
 export default page;
