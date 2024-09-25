d9a2cc060e13064bcabb664beafe2115d83833a1
shahidx0x <dev.imshahid@gmail.com>
2024-09-25 07:04:27 +0600

    Refactor metadata in layout.tsx and update EraVend System information

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -5,16 +5,39 @@ import { Metadata } from "next";
 import { ThemeProvider } from "../provider/theme-provider";
 import "./global.css";
 
+
 export const metadata: Metadata = {
-  title: "EraVend CRM",
-  description: "This is the EraVend's CRM System.",
+  title: "EraVend System",
+  description: "This is the EraVend's All in One System.",
+  openGraph: {
+    title: "EraVend System",
+    description: "This is the EraVend's CRM System.",
+    url: "./logo_white.png",
+    siteName: "EraVend",
+    images: [
+      {
+        url: "./logo_white.png",
+        width: 1200,
+        height: 630,
+        alt: "EraVend CRM",
+      },
+    ],
+    type: "website",
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "EraVend System",
+    description: "This is the EraVend's All in One System.",
+    images: ["./logo_white.png"],
+  },
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body>
