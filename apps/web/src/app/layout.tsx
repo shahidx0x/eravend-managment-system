efb1cf27101a8c99482883153e9a3a4c238855d8
shahidx0x <dev.imshahid@gmail.com>
2024-09-28 12:33:18 +0600

    Refactor metadata URLs in layout.tsx

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -5,18 +5,17 @@ import { Metadata } from "next";
 import { ThemeProvider } from "../provider/theme-provider";
 import "./global.css";
 
-
 export const metadata: Metadata = {
   title: "EraVend System",
   description: "This is the EraVend's All in One System.",
   openGraph: {
     title: "EraVend System",
     description: "This is the EraVend's CRM System.",
-    url: "./logo_white.png",
+    url: "https://i.ibb.co.com/d2dFyTw/logo-white.png",
     siteName: "EraVend",
     images: [
       {
-        url: "./logo_white.png",
+        url: "https://i.ibb.co.com/d2dFyTw/logo-white.png",
         width: 1200,
         height: 630,
         alt: "EraVend CRM",
@@ -29,10 +28,9 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "EraVend System",
     description: "This is the EraVend's All in One System.",
-    images: ["./logo_white.png"],
+    images: ["https://i.ibb.co.com/d2dFyTw/logo-white.png"],
   },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
