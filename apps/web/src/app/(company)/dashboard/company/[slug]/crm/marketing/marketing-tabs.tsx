e5f4b158693c322d9e3e0dcb26b3de0d81f9b246
Md. Sazedur Rahman <37084945+t3ng221@users.noreply.github.com>
2024-10-05 18:57:55 +0600

    add search,export,add campaign, campaigns table,pagination

---
+++ apps/web/src/app/(company)/dashboard/company/[slug]/crm/marketing/marketing-tabs.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+
+export default function MarketingTabs() {
+  return (
+    <div className="container p-4">
+      <Tabs defaultValue="active-campaign">
+        <div className="w-full overflow-auto">
+          <TabsList className="grid w-full min-w-[800px] grid-cols-5">
+            <TabsTrigger value="active-campaign">Active Campaign</TabsTrigger>
+            <TabsTrigger value="completed-campaign">
+              Completed Campagin
+            </TabsTrigger>
+            <TabsTrigger value="archieved-campaign">
+              Archieved Campaign
+            </TabsTrigger>
+          </TabsList>
+        </div>
+        <TabsContent value="active-campaign"></TabsContent>
+        <TabsContent value="completed-campaign"></TabsContent>
+        <TabsContent value="archieved-campaign"></TabsContent>
+      </Tabs>
+    </div>
+  );
+}
